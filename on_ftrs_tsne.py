5df2b70e740a7c8015b778dc0227a42247fb1b23
Mohammad Erfani <serfani@email.sc.edu>
2021-08-06 09:43:31 -0400

    Modified the name of input files.

--- on_ftrs_tsne.py
+++ on_ftrs_tsne.py
@@ -4,15 +4,15 @@ from sklearn.manifold import TSNE
 from models.tsne import extract_sequence
 from utils.visualize import savegif
 
-git dataset = ATeX()
-features = np.loadtxt('./outputs/train_tsne2d_shufflenet_ftrs.txt', delimiter=',')
-labels = np.loadtxt('./outputs/train_tsne2d_shufflenet_lbls.txt', delimiter=',')
+dataset = ATeX()
+features = np.loadtxt('./outputs/train_shufflenet_ftrs.txt', delimiter=',')
+labels = np.loadtxt('./outputs/train_shufflenet_lbls.txt', delimiter=',')
 
-perplexity=20
-learning_rate=200
-n_iter=300
-exploration_n_iter=100
-method="learning_rate"
+perplexity = 20
+learning_rate = 200
+n_iter = 300
+exploration_n_iter = 100
+method = "learning_rate"
 
 tsne = TSNE(perplexity=perplexity, learning_rate=learning_rate,
             n_iter=n_iter, verbose=2, method=method)
@@ -24,6 +24,6 @@ lo = Y_seq.min(axis=0).min(axis=0).max()
 hi = Y_seq.max(axis=0).max(axis=0).min()
 limits = ([lo, hi], [lo, hi])
 
-fig_name = "%s-%d-%d-tsne" %("ATeX", 300, 100)
-fig_path = "./outputs/%s.gif" %(fig_name)
+fig_name = "%s-%d-%d-tsne" % ("ATeX", 300, 100)
+fig_path = "./outputs/%s.gif" % (fig_name)
 savegif(Y_seq, labels, "t-SNE", fig_path, dataset.classes, limits=limits)
