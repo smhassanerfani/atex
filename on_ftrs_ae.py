98e93f66069df930101cac085125818655536b06
Mohammad Erfani <serfani@email.sc.edu>
2021-08-06 09:40:03 -0400

    Modified the name of input files.

--- on_ftrs_ae.py
+++ on_ftrs_ae.py
@@ -7,9 +7,9 @@ from torch.optim import lr_scheduler
 from models.ae import AELinear
 from utils.visualize import savegif
 
-git dataset = ATeX()
-features = np.loadtxt('./outputs/train_tsne2d_shufflenet_ftrs.txt', delimiter=',')
-labels = np.loadtxt('./outputs/train_tsne2d_shufflenet_lbls.txt', delimiter=',')
+dataset = ATeX()
+features = np.loadtxt('./outputs/train_shufflenet_ftrs.txt', delimiter=',')
+labels = np.loadtxt('./outputs/train_shufflenet_lbls.txt', delimiter=',')
 
 features = torch.from_numpy(features.astype(np.float32))
 features = DataLoader(features, batch_size=64, shuffle=False, drop_last=False)
@@ -36,15 +36,15 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
- 
+
         if idx == 0:
             ftrs_list = ftrs.cpu().detach().numpy()
             continue
         ftrs_list = np.vstack((ftrs_list, ftrs.cpu().detach().numpy()))
- 
+
     scheduler.step()
     ftrs_list = np.asarray(ftrs_list)
- 
+
     ftrs_per_epoch.append(ftrs_list)
     print(f"Epoch: {epoch+1}, Loss: {loss.item():.4f}")
 
@@ -52,7 +52,7 @@ save_path = "./outputs/ae-lin8/model.pth"
 torch.save(model.state_dict(), save_path)
 
 Y_seq = np.array(ftrs_per_epoch)
- 
+
 with open('outputs/ae-lin8/atex_train.pkl', 'wb') as f:
     pickle.dump(Y_seq, f)
 
@@ -68,4 +68,5 @@ limits = ([lo, hi], [lo, hi])
 
 fig_name = "{dataset_name}-ae".format(dataset_name="ATeX")
 fig_path = "./outputs/ae-lin8/{file_name}.gif".format(file_name=fig_name)
-savegif(Y_seq, labels, "Linear Auto Encoder", fig_path, dataset.classes, limits=limits)
+savegif(Y_seq, labels, "Linear Auto Encoder",
+        fig_path, dataset.classes, limits=limits)
