ab7bb3ba802006d1b9edfb32a23cc28154303861
Mohammad Erfani <serfani@email.sc.edu>
2021-08-05 21:33:14 -0400

    Added PCA and LDA features reductions techniques.

---
+++ on_ftrs_pca-lda.py
@@ -0,0 +1,20 @@
+import numpy as np
+from dataloader import ATeX
+from utils.pca import PCA
+# from utils.lda import LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from utils.visualize import plot_2d
+
+dataset = ATeX()
+features = np.loadtxt('./outputs/train_tsne2d_shufflenet_ftrs.txt', delimiter=',')
+labels = np.loadtxt('./outputs/train_tsne2d_shufflenet_lbls.txt', delimiter=',')
+
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_r = lda.fit(features, labels).transform(features)
+plot_2d(X_r, labels, dataset.classes)
+
+pca = PCA(2)
+pca.fit(features)
+X_r = pca.transform(features)
+plot_2d(X_r, labels, dataset.classes)
