c6eac361457482955d4162d87dbaf37962c4e713
Mohammad Erfani <serfani@email.sc.edu>
2021-08-11 11:58:53 -0400

    Extracted val set features.

--- ftrs_extraction.py
+++ ftrs_extraction.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from models.shufflenet import FeatureExtractor as shuff_fe
@@ -12,8 +13,8 @@ mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 transforms_list = [transforms.ToTensor(), transforms.Normalize(*mean_std)]
 transforms = transforms.Compose(transforms_list)
 
-dataset = ATeX(split="train", transform=transforms)
-atex = DataLoader(dataset, batch_size=64, shuffle=False, drop_last=False)
+dataset = ATeX(split="val", transform=transforms)
+atex = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False)
 
 
 model = initialize_model("shufflenet", 15)
@@ -35,15 +36,16 @@ features_list = []
 for inputs, labels, class_names in atex:
 
     inputs = inputs.to(device)
-    # labels_list.append(labels.item())
-    labels_list.append(labels.cpu().detach().numpy())
+    labels_list.append(labels.item())
+    # labels_list.append(labels.cpu().detach().numpy())
     with torch.no_grad():
         features = new_model(inputs)
-        features_list.append(features.cpu().detach().numpy())
+        features_list.append(features.cpu().detach().numpy().reshape(-1))
 
 
 features = np.asarray(features_list)
 labels = np.asarray(labels_list)
 
-np.savetxt('./outputs/train_shufflenet_ftrs.txt', features, delimiter=',')
-np.savetxt('./outputs/train_shufflenet_lbls.txt', labels, delimiter=',')
+print(features.shape)
+# np.savetxt('./outputs/val_shufflenet_ftrs.txt', features, delimiter=',')
+np.savetxt('./outputs/val_shufflenet_lbls.txt', labels, delimiter=',')
