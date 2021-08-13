5709b433190ce277cf00f07f0557ca9c992337cd
Mohammad Erfani <serfani@email.sc.edu>
2021-08-13 13:09:00 -0400

    Modified for AECONV4.

--- on_imgs_ae.py
+++ on_imgs_ae.py
@@ -6,26 +6,17 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from dataloader import ATeX
-from models.ae import AEConv3
+from models.ae import AEConv3, AEConv4
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-transforms_list = [transforms.ToTensor(), transforms.Normalize(*mean_std)]
+# mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+# transforms_list = [transforms.ToTensor(), transforms.Normalize(*mean_std)]
 transforms_list = [transforms.ToTensor()]
 transforms = transforms.Compose(transforms_list)
 
 dataset = ATeX(split="val", transform=transforms)
-atex = DataLoader(dataset, batch_size=64, shuffle=False, drop_last=False)
-
-# def lr_poly(base_lr, iter, max_iter, power):
-#     return base_lr * ((1 - float(iter) / max_iter) ** (power))
-
-
-# def adjust_learning_rate(optimizer, lr):
-#     optimizer.param_groups[0]['lr'] = lr
-#     if len(optimizer.param_groups) > 1:
-#         optimizer.param_groups[1]['lr'] = lr * 10
+atex = DataLoader(dataset, batch_size=256, shuffle=False, drop_last=False)
 
 
 model = AEConv3()
@@ -35,7 +26,7 @@ scheduler = lr_scheduler.StepLR(optimizer, step_size=70, gamma=0.1)
 
 model.to(device)
 
-num_epochs = 200
+num_epochs = 250
 outputs = []
 for epoch in range(num_epochs):
     for (img, _, _) in atex:
@@ -50,25 +41,26 @@ for epoch in range(num_epochs):
         optimizer.step()
 
     scheduler.step()
-    print(f"Epoch: {epoch+1}, Loss: {loss.item():.4f}")
+    print(f"Epoch: {epoch+1:3d}, Loss: {loss.item():.4f}")
     outputs.append((epoch, img, recon))
+
 
 for k in range(0, num_epochs, 20):
     plt.figure()
     imgs = outputs[k][1].detach().cpu().numpy()
     recon = outputs[k][2].detach().cpu().numpy()
     for i, item in enumerate(imgs):
-        if i >= 9:
+        if i >= 20:
             break
-        plt.subplot(2, 9, i + 1)
+        plt.subplot(2, 20, i + 1)
         item = item.transpose((1, 2, 0))
         # item = 0.229 * item + 0.485
         plt.axis('off')
         plt.imshow(item)
     for i, item in enumerate(recon):
-        if i >= 9:
+        if i >= 20:
             break
-        plt.subplot(2, 9, 9 + i + 1)
+        plt.subplot(2, 20, 20 + i + 1)
         item = item.transpose((1, 2, 0))
         # item = 0.229 * item + 0.485
         plt.axis('off')
