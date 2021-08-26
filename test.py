e3df5f17363e3b78c72a473f43fc86e44ef0191a
Mohammad Erfani <serfani@email.sc.edu>
2021-08-26 13:50:08 -0400

    Added test.py for high-level vision analysis.

---
+++ test.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+from dataloader import ATeX
+from utils.initialize_model import initialize_model
+from sklearn.metrics import classification_report
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+transforms_list = [transforms.ToTensor(), transforms.Normalize(*mean_std)]
+transforms = transforms.Compose(transforms_list)
+
+dataset = ATeX(split='test', transform=transforms)
+atex = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False)
+
+class_names = dataset.classes
+# print(class_names)
+
+model_name = "resnet"
+model = initialize_model(model_name, num_classes=15, use_pretrained=True)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=1.0e-2,
+                      momentum=0.9, weight_decay=0.0001)
+
+FILE = f"./outputs/models/{model_name}/model.pth"
+
+checkpoint = torch.load(FILE)
+model.load_state_dict(checkpoint['model_state'])
+optimizer.load_state_dict(checkpoint['optimizer_state'])
+# scheduler.load_state_dict(checkpoint['scheduler_state'])
+epoch = checkpoint['epoch']
+
+model.to(device)
+model.eval()
+
+y_true = []
+y_pred = []
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(len(class_names))]
+    n_class_samples = [0 for i in range(len(class_names))]
+    for image, label, _ in atex:
+
+        image = image.to(device)
+        label = label.to(device)
+        output = model(image)
+
+        _, predicted = torch.max(output, 1)
+        n_samples += label.size(0)
+        n_correct += (predicted == label).sum().item()
+
+        y_true.append(label.item())
+        y_pred.append(predicted.item())
+
+
+final_report = classification_report(y_true, y_pred, target_names=class_names)
+
+
+print(final_report)
+
+# with open(f".output/models/{model_name}/final_report.txt", "a") as fh:
+#     fh.write(final_report)
