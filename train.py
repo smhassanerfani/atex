78325634857b33f76d4beba5fbbf243c9a34aa85
Mohammad Erfani <serfani@email.sc.edu>
2021-08-30 10:40:59 -0400

    Added DRNet to script.

--- train.py
+++ train.py
@@ -11,6 +11,9 @@ from dataloader import ATeX
 from utils.initialize_model import initialize_model
 from utils.engines import train_model
 
+from models.drn import ResNet101
+RESTORE_FROM = "/home/serfani/Downloads/resnet101-imagenet.pth"
+# RESTORE_FROM = "/home/serfani/Downloads/fcn_resnet101_coco-7ecb50ca.pth"
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -26,14 +29,28 @@ atex = {x: DataLoader(dataset[x], batch_size=64, shuffle=True,
 # class_names = dataset['train'].classes
 # print(class_names)
 
-model_name = "resnet-101"
+model_name = "drn-101"
 
 try:
     os.makedirs(os.path.join("./outputs/models", model_name))
 except FileExistsError:
     pass
 
-model = initialize_model(model_name, num_classes=15, use_pretrained=True)
+# model = initialize_model(model_name, num_classes=15, use_pretrained=True)
+model = ResNet101(img_channel=3, num_classes=15)
+
+saved_state_dict = torch.load(RESTORE_FROM)
+new_params = model.state_dict().copy()
+
+for key, value in saved_state_dict.items():
+    if (key.split(".")[0] not in ["head", "dsn", "fc"]):
+        # print(key)
+        new_params[key] = value
+
+model.load_state_dict(new_params, strict=False)
+# print(model)
+
+
 model = model.to(device)
 
 # MODEL INFORMATION
@@ -46,7 +63,7 @@ optimizer = optim.SGD(model.parameters(), lr=2.5e-4,
                       momentum=0.9, weight_decay=0.0001)
 
 # optimizer = torch.optim.Adam(model.parameters(), lr=2.5e-4)
-# step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+# step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
 model = train_model(model, model_name, atex,
-                    criterion, optimizer, num_epochs=30)
+                    criterion, optimizer, 2.5e-4, scheduler=None, num_epochs=30)
