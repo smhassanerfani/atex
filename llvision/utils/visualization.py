3683134656030a686e35f169bb55f88a83074108
Mohammad Erfani <serfani@email.sc.edu>
2021-09-01 10:20:40 -0400

    Added font settings.

--- llvision/utils/visualization.py
+++ llvision/utils/visualization.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# import matplotlib
+
 
 def plot_samples(dataset, num_of_samples=7):
+
+    font = {'font.family': 'Times New Roman', 'font.size': 14}
+    plt.rcParams.update(**font)
 
     for y, cls in enumerate(dataset["classes"]):
         idxs = np.flatnonzero(dataset["train"]["target"] == y)
@@ -9,7 +14,7 @@ def plot_samples(dataset, num_of_samples=7):
         for i, idx in enumerate(idxs):
             plt_idx = i * len(dataset["classes"]) + y + 1
             plt.subplot(num_of_samples, len(dataset["classes"]), plt_idx)
-            plt.imshow(atex["train"]["data"][idx].astype('uint8'))
+            plt.imshow(dataset["train"]["data"][idx].astype('uint8'))
             plt.axis('off')
             if i == 0:
                 plt.title(cls)
