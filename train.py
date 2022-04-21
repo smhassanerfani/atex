c306adb8950b790392d4eab6e86ebf11ed428e6b
Mohammad Erfani <serfani@email.sc.edu>
2022-04-21 14:53:00 -0400

    Trained RepVGG

--- train.py
+++ train.py
@@ -78,20 +78,16 @@ model = model.to(device)
 # print(model)
 # exit()
 
-# Babysitting the learning process
-max_count = 100
-for count in range(max_count):
+# base_lr = 10**random.uniform(-3, -6)
+base_lr = 8.50E-03
 
+# criterion = FocalLoss()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=base_lr,
+                      momentum=0.9, weight_decay=0.0001)
 
-    base_lr = 10**random.uniform(-3, -6)
+# optimizer = torch.optim.Adam(model.parameters(), lr=2.5e-4)
+# step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 
-    # criterion = FocalLoss()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=base_lr,
-                          momentum=0.9, weight_decay=0.0001)
-
-    # optimizer = torch.optim.Adam(model.parameters(), lr=2.5e-4)
-    # step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
-
-    model = train_model(model, model_name, atex,
-                        criterion, optimizer, base_lr, pdlr=False, scheduler=None, num_epochs=5)
+model = train_model(model, model_name, atex,
+                    criterion, optimizer, base_lr, pdlr=True, scheduler=None, num_epochs=50)
